59faec0075907ba7b4a3ae68dbf571a87734919f
Gopendra Kumar <gopendra1.kumar@gmial.com>
2022-07-22 00:42:11 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import Title from './components/layout/Title';
+import FlowContainer from './containers/FlowContainer';
+import { ImageMapEditor } from './editors';
+
+class App extends Component<any> {
+
+	render() {
+		return (
+			<div className="rde-main">
+				<Helmet>
+					<meta charSet="utf-8" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+					<meta
+						name="description"
+						content="React Design Editor has started to developed direct manipulation of editable design tools like Powerpoint, We've developed it with react.js, ant.design, fabric.js "
+					/>
+					<link rel="manifest" href="./manifest.json" />
+					<link rel="shortcut icon" href="./favicon.ico" />
+					<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosanskr.css" />
+					<title>React Design Editor</title>
+					<script async={true} src="https://www.googletagmanager.com/gtag/js?id=UA-97485289-3" />
+					<script>
+						{`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'UA-97485289-3');
+                        `}
+					</script>
+					<script async={true} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+				</Helmet>
+				<div className="rde-title">
+					<Title />
+				</div>
+				<FlowContainer>
+					<div className="rde-content"><ImageMapEditor /></div>
+				</FlowContainer>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/layout/Title.tsx
@@ -0,0 +1,99 @@
+import { Button, Menu, Modal, Tooltip } from 'antd';
+import { ClickParam } from 'antd/lib/menu';
+import i18next from 'i18next';
+import React, { Component } from 'react';
+import { Flex } from '../flex';
+import { ShortcutHelp } from '../help';
+import Icon from '../icon/Icon';
+
+class Title extends Component {
+	state = {
+		visible: false,
+	};
+
+	componentDidMount() {
+		if (globalThis) {
+			(globalThis.adsbygoogle = globalThis.adsbygoogle || []).push({});
+		}
+	}
+
+	handlers = {
+		goGithub: () => {
+			window.open('https://github.com/salgum1114/react-design-editor');
+		},
+		goDocs: () => {
+			window.open('https://salgum1114.github.io/react-design-editor/docs');
+		},
+		showHelp: () => {
+			this.setState({
+				visible: true,
+			});
+		},
+	};
+
+	render() {
+		const { visible } = this.state;
+		return (
+			<Flex
+				style={{ background: 'linear-gradient(141deg,#23303e,#404040 51%,#23303e 75%)' }}
+				flexWrap="wrap"
+				flex="1"
+				alignItems="center"
+			>
+				<Flex style={{ marginLeft: 8 }} flex="0 1 auto">
+					<span style={{ color: '#fff', fontSize: 24, fontWeight: 500 }}>React Design Editor</span>
+					<Tooltip title={i18next.t('action.go-github')} overlayStyle={{ fontSize: 16 }}>
+						<Button
+							className="rde-action-btn"
+							style={{
+								color: 'white',
+							}}
+							shape="circle"
+							size="large"
+							onClick={this.handlers.goGithub}
+						>
+							<Icon name="github" prefix="fab" size={1.5} />
+						</Button>
+					</Tooltip>
+					<Tooltip title={i18next.t('action.go-docs')} overlayStyle={{ fontSize: 16 }}>
+						<Button
+							className="rde-action-btn"
+							style={{
+								color: 'white',
+							}}
+							shape="circle"
+							size="large"
+							onClick={this.handlers.goDocs}
+						>
+							<Icon name="book" prefix="fas" size={1.5} />
+						</Button>
+					</Tooltip>
+					<Tooltip title={i18next.t('action.shortcut-help')} overlayStyle={{ fontSize: 16 }}>
+						<Button
+							className="rde-action-btn"
+							style={{
+								color: 'white',
+							}}
+							shape="circle"
+							size="large"
+							onClick={this.handlers.showHelp}
+						>
+							<Icon name="question" prefix="fas" size={1.5} />
+						</Button>
+					</Tooltip>
+				</Flex>
+				<Modal
+					visible={visible}
+					onCancel={() => this.setState({ visible: false })}
+					closable={true}
+					footer={null}
+					width="50%"
+				>
+					<ShortcutHelp />
+				</Modal>
+			</Flex>
+		);
+	}
+}
+
+export default Title;
